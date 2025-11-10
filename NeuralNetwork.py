1d0dec95ea1177472f780d259c2ec15c621cef20
gwurgaft <g.t.wurgaft@wustl.edu>
2025-11-10 14:02:02 -0600

    full dataset

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -1,6 +1,26 @@
+import torch
 import torch.nn as nn
 import torch.optim as optim
+from torch.utils.data import Dataset, Dataloader
+import pandas as pd
 
+df = pd.read_csv("final_dataset1.csv")
+y = df['volatility'].values
+X = df.loc[:, df.columns != 'volatility'].values
+
+X_tensor = torch.tensor(X, dtype=torch.float32)
+y_tensor = torch.tensor(y, dtype=torch.float)
+
+
+class CSVDataset(Dataset):
+    def __init__(self, X, y):
+        self.X = X
+        self.y = y
+    def __len__(self):
+        return len(self.X)
+    def __getitem__(self, idx):
+        return self.X[idx], self.y[idx]
+    
 class SimpleNN(nn.Module):
     def __init__(self, input_size):
         super(SimpleNN, self).__init__()
@@ -18,3 +38,4 @@ class SimpleNN(nn.Module):
         x = self.flatten(x)
         logits = self.linear_relu_stack(x)
         return logits
+dataset = CSVDataset(X_tensor, y_tensor)
